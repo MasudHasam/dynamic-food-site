8fd8224b71574a9fedeb74ba966d4b6b5aa1fa19
Masud Hasan <masudhasamf@gmail.com>
2022-08-29 20:53:30 +0600

    show food details on display if some one click on it and async and await done

--- food.js
+++ food.js
@@ -14,7 +14,7 @@ const displayFood = (loadedFoods) => {
     loadedFoods.forEach(food => {
         const div = document.createElement('div');
         div.innerHTML = `
-       <div class="col">
+       <div onclick="loadedFoodDetail(${food.idMeal})" class="col">
              <div class="card h-100">
                  <img src="${food.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -35,4 +35,40 @@ const searchButton = () => {
     const inputValueText = getInputValue.value;
     loadFood(inputValueText)
     getInputValue.value = '';
+}
+
+
+// //  this function is for show loaded food details.
+// const loadedFoodDetails = (idMeal) => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displayFoodDetails(data.meals))
+//         //catch an error by this code and console log it.
+//         .catch(error => console.log(error));
+// }
+// Or we can also use async and await to replace fetch. (it's a alternative for fetch.)
+const loadedFoodDetail = async (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayFoodDetails(data.meals);
+    ///hear we can use try and catch to manage an error.
+}
+
+
+//this function is for display food details.
+const displayFoodDetails = (foodDetails) => {
+    const foodDetailsContainer = document.getElementById('foodDetailsContainer');
+    foodDetailsContainer.innerHTML = '';
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card" style="width: 30rem;">
+        <img src="${foodDetails[0].strMealThumb}" class="card-img-top"  alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-center">${foodDetails[0].strMeal}</h5>
+        </div>
+    </div>
+    `
+    foodDetailsContainer.appendChild(div);
 }
